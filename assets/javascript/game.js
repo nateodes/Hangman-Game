767b8bf9af8415e402818081d4fac140b6ecab6f
Nate Odes <Nate Odes>
2017-05-15 20:10:51 -0600

    letter function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 				];
 var currentWord = ["deathstar", "Yoda", "MilleniumFalcon", "JediKnight", "HanSolo", "TheForceAwakens", "ANewHope", "Chewbacca", "DarthMaul", "CountDooku"];
 
-
+var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
 
 // Creat Puzzle Function 
 
@@ -26,7 +26,35 @@ document.getElementById("start").addEventListener("click", function(word) {
 
 document.getElementById("start").addEventListener("click", function(hide){
 			document.getElementById("wordWrap").style.visibility = "hidden";
-
+			document.getElementById("letterA").style.visibility = "hidden";
+			document.getElementById("letterB").style.visibility = "hidden";
+			document.getElementById("letterC").style.visibility = "hidden";
+			document.getElementById("letterD").style.visibility = "hidden";
+			document.getElementById("letterE").style.visibility = "hidden";
+			document.getElementById("letterF").style.visibility = "hidden";
+			document.getElementById("letterG").style.visibility = "hidden";
+			document.getElementById("letterH").style.visibility = "hidden";
+			document.getElementById("letterI").style.visibility = "hidden";
+			document.getElementById("letterG").style.visibility = "hidden";
+			document.getElementById("letterH").style.visibility = "hidden";
+			document.getElementById("letterI").style.visibility = "hidden";
+			document.getElementById("letterJ").style.visibility = "hidden";
+			document.getElementById("letterK").style.visibility = "hidden";
+			document.getElementById("letterL").style.visibility = "hidden";
+			document.getElementById("letterM").style.visibility = "hidden";
+			document.getElementById("letterN").style.visibility = "hidden";
+			document.getElementById("letterO").style.visibility = "hidden";
+			document.getElementById("letterP").style.visibility = "hidden";
+			document.getElementById("letterQ").style.visibility = "hidden";
+			document.getElementById("letterR").style.visibility = "hidden";
+			document.getElementById("letterS").style.visibility = "hidden";
+			document.getElementById("letterT").style.visibility = "hidden";
+			document.getElementById("letterU").style.visibility = "hidden";
+			document.getElementById("letterV").style.visibility = "hidden";
+			document.getElementById("letterW").style.visibility = "hidden";
+			document.getElementById("letterX").style.visibility = "hidden";
+			document.getElementById("letterY").style.visibility = "hidden";
+			document.getElementById("letterZ").style.visibility = "hidden";
 
 			console.log("word is hidden!")
 }
@@ -35,6 +63,20 @@ document.getElementById("start").addEventListener("click", function(hide){
 	
 	);
 
+	
+
+
+	
+	     document.onkeypress = function(letter) {
+		 
+			var x = event.charCode;
+    		var res = String.fromCharCode(x);
+
+   			document.getElementById("guesses").innerHTML = res;
+   			
+   			console.log(res);
+
+};
 
 
 
