f6ce2939bccc17d021356c940ae52760698919f3
Nate Odes <Nate Odes>
2017-05-18 21:00:58 -0600

    array display works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,82 +3,150 @@
 
 
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-				];
-var currentWord = ["deathstar", "Yoda", "MilleniumFalcon", "JediKnight", "HanSolo", "TheForceAwakens", "ANewHope", "Chewbacca", "DarthMaul", "CountDooku"];
+
+var currentWord = [
+["D", "E", "A", "T", "H", "S", "T", "A", "R"],
+["C", "H", "E", "W", "B", "A", "C", "C", "A"],
+["S", "K", "Y", "W", "A", "L", "K", "E", "R"],
+["E", "W", "O", "K"],
+["J", "E", "D", "I"],
+["Y", "O", "D", "A"],
+
+
+]
+
 
 var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
 
-// Creat Puzzle Function 
 
-function puzzle () {
-	var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
+$("#start").on("click", function() {
+	//var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
+	var wordDiv = $("#word");
+	//document.getElementById("word").style.visibility = "hidden";
+	for (var i = 0; i < computerGuess.length; i++) {
+
+
+      var newWordDiv = $("<div>" + " __ " + "</div>");
+      computerGuess[i] = " __ ";
+      $("#word").html(computerGuess);
+      
+      //wordDiv.append(newWordDiv);
+ }}
+  )
+$("#lettera").on("click", function (){
+	console.log("you clicked A")
+	//var guess = computerGuess;
+	for (var i = computerGuess[0]; i < computerGuess.length; i++) {
+			 
+			if (i === "A") {
+			
+				computerGuess[i] = "A";
+				$("#word").html("A");
+				console.log("you clicked A if");
+	}}
+		
+			
+		
 	
-}
+});
 
-document.getElementById("start").addEventListener("click", function(word) {
-	var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
-	document.getElementById("wordWrap").innerHTML = computerGuess;
+
+
+
+
+
+
+
+//$("#start").on("click", function(word) {
+	
+//	$("#wordWrap").html(computerGuess);
 	//document.querySelector("#wordWrap").style.letterSpacing = 3px; 
-	console.log("word is here!");
-} 
- );
+	
+//	console.log("word is here!");
 
-document.getElementById("start").addEventListener("click", function(hide){
-			document.getElementById("wordWrap").style.visibility = "hidden";
-			document.getElementById("letterA").style.visibility = "hidden";
-			document.getElementById("letterB").style.visibility = "hidden";
-			document.getElementById("letterC").style.visibility = "hidden";
-			document.getElementById("letterD").style.visibility = "hidden";
-			document.getElementById("letterE").style.visibility = "hidden";
-			document.getElementById("letterF").style.visibility = "hidden";
-			document.getElementById("letterG").style.visibility = "hidden";
-			document.getElementById("letterH").style.visibility = "hidden";
-			document.getElementById("letterI").style.visibility = "hidden";
-			document.getElementById("letterG").style.visibility = "hidden";
-			document.getElementById("letterH").style.visibility = "hidden";
-			document.getElementById("letterI").style.visibility = "hidden";
-			document.getElementById("letterJ").style.visibility = "hidden";
-			document.getElementById("letterK").style.visibility = "hidden";
-			document.getElementById("letterL").style.visibility = "hidden";
-			document.getElementById("letterM").style.visibility = "hidden";
-			document.getElementById("letterN").style.visibility = "hidden";
-			document.getElementById("letterO").style.visibility = "hidden";
-			document.getElementById("letterP").style.visibility = "hidden";
-			document.getElementById("letterQ").style.visibility = "hidden";
-			document.getElementById("letterR").style.visibility = "hidden";
-			document.getElementById("letterS").style.visibility = "hidden";
-			document.getElementById("letterT").style.visibility = "hidden";
-			document.getElementById("letterU").style.visibility = "hidden";
-			document.getElementById("letterV").style.visibility = "hidden";
-			document.getElementById("letterW").style.visibility = "hidden";
-			document.getElementById("letterX").style.visibility = "hidden";
-			document.getElementById("letterY").style.visibility = "hidden";
-			document.getElementById("letterZ").style.visibility = "hidden";
 
-			console.log("word is hidden!")
-}
+//} 
+// );
+
+// document.getElementById("start").addEventListener("click", function(hide){
+// 			//document.getElementById("word").style.visibility = "hidden";
+// 			document.getElementById("lettera").style.visibility = "hidden";
+// 			document.getElementById("letterb").style.visibility = "hidden";
+// 			document.getElementById("letterc").style.visibility = "hidden";
+// 			document.getElementById("letterd").style.visibility = "hidden";
+// 			document.getElementById("lettere").style.visibility = "hidden";
+// 			document.getElementById("letterf").style.visibility = "hidden";
+// 			document.getElementById("letterg").style.visibility = "hidden";
+// 			document.getElementById("letterh").style.visibility = "hidden";
+// 			document.getElementById("letteri").style.visibility = "hidden";
+// 			document.getElementById("letterj").style.visibility = "hidden";
+// 			document.getElementById("letterk").style.visibility = "hidden";
+// 			document.getElementById("letterl").style.visibility = "hidden";
+// 			document.getElementById("letterm").style.visibility = "hidden";
+// 			document.getElementById("lettern").style.visibility = "hidden";
+// 			document.getElementById("lettero").style.visibility = "hidden";
+// 			document.getElementById("letterp").style.visibility = "hidden";
+// 			document.getElementById("letterq").style.visibility = "hidden";
+// 			document.getElementById("letterr").style.visibility = "hidden";
+// 			document.getElementById("letters").style.visibility = "hidden";
+// 			document.getElementById("lettert").style.visibility = "hidden";
+// 			document.getElementById("letteru").style.visibility = "hidden";
+// 			document.getElementById("letterv").style.visibility = "hidden";
+// 			document.getElementById("letterw").style.visibility = "hidden";
+// 			document.getElementById("letterx").style.visibility = "hidden";
+// 			document.getElementById("lettery").style.visibility = "hidden";
+// 			document.getElementById("letterz").style.visibility = "hidden";
+// 			//document.getElementById("letterX").style.visibility = "hidden";
+// 			//document.getElementById("letterY").style.visibility = "hidden";
+// 			//document.getElementById("letterZ").style.visibility = "hidden";
+
+// 			console.log("word is hidden!")
+// }
+
+
+
+	//);
+
+	
 
 
 	
-	);
+// 	     document.onkeypress = function() {
+// 		 	var wins = 0;
+// 		 	var guesses = 15;
+// 			var x = event.charCode;
+//     		var res = String.fromCharCode(x);
+//     		guesses--
+//    			//document.getElementById("guesses").innerHTML = res;
+//    			var y = res.toUpperCase();
+//    			console.log(res);
+//    			var computerGuess = $("#word");
+//    			if (computerGuess.includes(y)) {
 
-	
+//    				computerGuess.replace(/ __ /g, y);
+//    				//document.getElementById("word").style.visibility = "visible";
 
+//    				console.log("you guessed correct");
 
-	
-	     document.onkeypress = function(letter) {
-		 
-			var x = event.charCode;
-    		var res = String.fromCharCode(x);
+//    			}
 
-   			document.getElementById("guesses").innerHTML = res;
-   			
-   			console.log(res);
+//    			else {
+//    				var guessedDiv = $("<div>" + y + "</div>");
 
-};
+//       // It then adds this new div to the drinkList div.
+//       wordDiv.append(guessedDiv);
+//       console.log("incorrect guess");
+//   };
 
+//    			var html = 
+//          	 "<p>wins: " + wins + "</p>" +
 
+//          	 "<p>guesses: " + guesses + "</p>";
+
+//          	 // Injecting the HTML we just created into our div and updating the game information on our page.
+//          	 document.querySelector("#guesses").innerHTML = html;
+
+// };
 
 
 //document.onkeyup = function(word) {
@@ -97,15 +165,15 @@ document.getElementById("start").addEventListener("click", function(hide){
 	//	var currentWord = ["deathstar", "Yoda", "MilleniumFalcon", "JediKnight", "HanSolo", "TheForceAwakens", "ANewHope", "Chewbacca", "DarthMaul", "CountDooku"];
 
 
-		
+
 	 //	var computerGuess = currentWord[Math.floor(Math.random() * currentWord.length)];
 		//console.log("puzzle function called")
-	
+
 		//var hidden = computerGuess.replace(/alphabet/gi, " _ ");
 
 		//for (i = computerGuess.substring(0); i < computerGuess.length;) {
 		//	computerGuess.substring(i).replace(i, " _ ")
-			
+
 		//	document.querySelector("#wordWrap").style.textDecoration = "underline";
 		//	i++;
 		//console.log("replace funtion works");
@@ -114,7 +182,7 @@ document.getElementById("start").addEventListener("click", function(hide){
 		//document.querySelector("#wordWrap").style.visibility = "hidden";
 		//document.querySelector("#wordWrap").style.borderBottom = "3px solid black";
 	//}	
-		
+
 		//
 
 		//if (letter = " ") {
